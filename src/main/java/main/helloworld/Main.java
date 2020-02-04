81a9f266827310561f237d79f53a092c42077691
Dejan <capricdejan@gmail.com>
2020-02-04 20:24:23 +0100

    adds age chacker

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -5,6 +5,8 @@
  */
 package main.helloworld;
 
+import java.util.Scanner;
+
 /**
  *
  * @author qa
@@ -17,6 +19,41 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Dejan");
+        
+        int age=7;
+        
+        if (age>18){
+          System.out.println("Osoba je punoletna");
+        } else {
+               System.out.println("Osoba je maloletna"); 
+                  }
+        System.out.print("---------------");
+        System.out.print("Unesite broj godina: ");
+        
+        Scanner input= new Scanner(System.in);
+        int userAge= input.nextInt();
+        
+         System.out.println("Korisnik je uneo:" + userAge);  
+         if (age>18){
+          System.out.println("Osoba je punoletna");
+        } else {
+               System.out.println("Osoba je maloletna"); 
+                  }
+          System.out.print("---------------");
+          System.out.print("Unesite broj godina: ");
+          input= new Scanner(System.in);
+          int personAge=input.nextInt();
+          
+          if (personAge>0 && personAge<=5){
+             System.out.println("Baby"); 
+          }else if(personAge<=11){
+              System.out.println("Kid");
+          }else if(personAge<=17){
+              System.out.println("Teen"); 
+          }else if(personAge>=18){
+          System.out.println("Adult");
+        }
     }
     
+
 }
